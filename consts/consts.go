7e6dd41c9dd60f44ed9f80a864ebbd87c5ab0b34
Nico <ainililia@163.com>
2021-12-27 02:52:18 +0800

    ss

--- consts/consts.go
+++ consts/consts.go
@@ -7,9 +7,9 @@ const (
 	M                   = 1024 * K
 	G                   = 1024 * M
 	FileBufferSize      = 256 * K
-	FileSortShardSize   = 512 * M
+	FileSortShardSize   = 64 * M
 	FileMergeBufferSize = 32 * M
 	InsertBatch         = 32 * K
-	FileSortLimit       = 2
+	FileSortLimit       = 4
 	SyncLimit           = 8
 )
