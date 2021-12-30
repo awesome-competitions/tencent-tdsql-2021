7970e0a008022d5d00f3a060022f3d9d242a0a51
Nico <ainililia@163.com>
2021-12-30 11:01:17 +0800

    不做限制

--- main.go
+++ main.go
@@ -53,8 +53,6 @@ func _main() {
 	if err != nil {
 		log.Panic(err)
 	}
-	tables = tables[:1]
-
 	fsChan := make(chan *filesort.FileSorter, len(tables))
 	sortLimit := make(chan bool, consts.FileSortLimit)
 	syncLimit := make(chan bool, consts.SyncLimit)
