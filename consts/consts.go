6bcd3d1f089ebca7aab037752d7401e2f3860d27
Nico <ainililia@163.com>
2022-01-01 18:06:05 +0800

    恢复

--- consts/consts.go
+++ consts/consts.go
@@ -10,7 +10,7 @@ const (
 	FileSortShardSize   = 512 * K
 	FileMergeBufferSize = 16 * M
 	InsertBatch         = 40 * K
-	FileSortLimit       = 4
+	FileSortLimit       = 2
 	SyncLimit           = 16
 	PreparedBatch       = 4
 )
