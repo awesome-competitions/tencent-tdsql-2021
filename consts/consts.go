15dc4363ac442e027a9531e5340ee02e4f0d9fa0
Nico <ainililia@163.com>
2022-01-03 22:46:43 +0800

    4 16

--- consts/consts.go
+++ consts/consts.go
@@ -9,8 +9,8 @@ const (
 	FileBufferSize      = 64 * K
 	FileSortShardSize   = 16 * M
 	FileMergeBufferSize = 32 * M
-	InsertBatch         = 50 * K
-	FileSortLimit       = 1
+	InsertBatch         = 40 * K
+	FileSortLimit       = 4
 	SyncLimit           = 100
 	PreparedBatch       = 4
 )
