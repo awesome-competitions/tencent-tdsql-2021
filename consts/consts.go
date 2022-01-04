200f53e3d7b6c7b880475ada80a92547a9f4fc8a
Nico <ainililia@163.com>
2022-01-04 11:15:50 +0800

    40k最稳？

--- consts/consts.go
+++ consts/consts.go
@@ -9,7 +9,7 @@ const (
 	FileBufferSize      = 64 * K
 	FileSortShardSize   = 32 * M
 	FileMergeBufferSize = 32 * M
-	InsertBatch         = 20 * K
+	InsertBatch         = 40 * K
 	FileSortLimit       = 2
 	SyncLimit           = 100
 	PreparedBatch       = 4
